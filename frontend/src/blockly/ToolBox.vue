f8febd20973f3afee9197320767639f11e217b1b
Tai8909 <timtai20000911@gmail.com>
2023-12-02 03:38:43 +0800

    Dynamically generate blocks of IR, State, Const.
    Set default value {} of blocklyWorkspace in device table.

--- frontend/src/blockly/ToolBox.vue
+++ frontend/src/blockly/ToolBox.vue
@@ -9,7 +9,15 @@
     'deviceId',
   ]);
 
-  let blocksValue;
+  const blocksValue = ref({
+    irCodes: {},
+    states: {},
+    consts: {},
+  });
+
+  function upperCase(str){
+    return str[0].toUpperCase()+str.slice(1);
+  }
 
   async function fetchBlocks(){
       /**
@@ -17,15 +25,111 @@
        */
       try {
         const result = await axios.get(`${globals.$origin}/api/blockly/${props.deviceId}`);
-        blocksValue = result.data;
-        console.log(blocksValue);
+        blocksValue.value = result.data;
+        // console.log(blocksValue.value);
       } catch(err) {
         console.error(err);
       }
   }
 
-  onMounted(()=>{
-    fetchBlocks();
+  function initBlocks(){
+    Blockly.Blocks['send_ir_code'] = {
+      init: function() {
+        this.appendValueInput("IR_CODE")
+            .setCheck("ir_code_id")
+            .appendField("Send IR code");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(30);
+        this.setTooltip("Send the IR code specified.");
+      }
+    }
+
+    javascriptGenerator.forBlock['send_ir_code'] = function(block, generator) {
+      var value_ir_code = generator.valueToCode(block, 'IR_CODE', javascript.Order.ATOMIC);
+      var code = '...\n';
+      return code;
+    };
+
+    Object.keys(blocksValue.value.irCodes).forEach((key) => {
+      Blockly.Blocks[`IR_${key}`] = {
+        init: function() {
+          this.appendDummyInput()
+              .appendField(blocksValue.value.irCodes[key].blockName);
+          this.setOutput(true, "ir_code_id");
+          this.setColour(30);
+          this.setTooltip(blocksValue.value.irCodes[key].blockDescription);
+        }
+      };
+
+      javascriptGenerator.forBlock[`IR_${key}`] = function(block, generator) {
+        var code = '...';
+        return [code, Blockly.javascript.ORDER_NONE];
+      };
+    });
+
+    Object.keys(blocksValue.value.states).forEach((key) => {
+      Blockly.Blocks[`State_${key}`] = {
+        init: function() {
+          this.appendDummyInput()
+              .appendField(blocksValue.value.states[key].blockName)
+              .appendField(new Blockly.FieldDropdown([["target state","TAR"], ["original state","ORI"]]), "TYPE");
+          this.setOutput(true, upperCase(blocksValue.value.states[key].type));
+          this.setColour(270);
+        }
+      };
+
+      javascriptGenerator.forBlock[`State_${key}`] = function(block, generator) {
+        var dropdown_type = block.getFieldValue('TYPE');
+        var code = '...';
+        return [code, Blockly.javascript.ORDER_NONE];
+      };
+    });
+
+    Object.keys(blocksValue.value.consts).forEach((key) => {
+      if(blocksValue.value.consts[key].type == 'array'){
+        const dropdown = blocksValue.value.consts[key].const.reduce((arr, element) => {
+          arr.push([element, element]);
+          return arr;
+        }, []);
+        console.log(dropdown);
+        Blockly.Blocks[`Const_${key}`] = {
+          init: function() {
+            this.appendDummyInput()
+                .appendField(blocksValue.value.consts[key].blockName)
+                .appendField(new Blockly.FieldDropdown(dropdown), "OPTION");
+            this.setOutput(true, "String");
+            this.setColour(150);
+          }
+        };
+
+        javascriptGenerator.forBlock[`Const_${key}`] = function(block, generator) {
+          var dropdown_option = block.getFieldValue('OPTION');
+          var code = '...';
+          return [code, Blockly.javascript.ORDER_NONE];
+        };
+      } else {
+        Blockly.Blocks[`Const_${key}`] = {
+          init: function() {
+            this.appendDummyInput()
+                .appendField(blocksValue.value.consts[key].blockName);
+            this.setOutput(true, upperCase(blocksValue.value.consts[key].type));
+            this.setColour(150);
+          }
+        };
+
+        javascriptGenerator.forBlock[`Const_${key}`] = function(block, generator) {
+          var code = '...';
+          return [code, Blockly.javascript.ORDER_NONE];
+        };
+      }
+    });
+  }
+
+  onMounted(async()=>{
+    await fetchBlocks();
+    await new Promise((res) => setTimeout(res, 250));
+    initBlocks();
   });
 </script>
 
@@ -172,7 +276,19 @@
         </value>
       </block>
     </category>
+    <sep></sep>
     <category name="Variables" categorystyle="variable_category" custom="VARIABLE"></category>
+    <sep></sep>
+    <category name="IR" colour="30">
+      <block type="send_ir_code"></block>
+      <block v-for="key in Object.keys(blocksValue.irCodes)" :type="`IR_${key}`"></block>
+    </category>
+    <category name="State" colour="270">
+      <block v-for="key in Object.keys(blocksValue.states)" :type="`State_${key}`"></block>
+    </category>
+    <category name="Const" colour="150">
+      <block v-for="key in Object.keys(blocksValue.consts)" :type="`Const_${key}`"></block>
+    </category>
   </xml>
 
 </template>
