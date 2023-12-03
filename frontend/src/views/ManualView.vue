7f70106f051bf6d2083ce4dab2c447bb25ad8ea0
RyanUnicorn <ryan112345ryan@gmail.com>
2023-12-04 02:29:00 +0800

    feat: make the left nav suitable for DEMO

--- frontend/src/views/ManualView.vue
+++ frontend/src/views/ManualView.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-    I'm manual view
+  <div class="center">
+    ASK THE PEOPLE IN FRONT OF YOU
+  </div>
 </template>
 
 <style scoped>
-
+  .center {
+    font-size: 3rem;
+    color: var(--color-text-mute);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-    I'm settings view
+  <div class="center">
+    NO SETTINGS FOR DEMO
+  </div>
 </template>
 
 <style scoped>
-
+  .center {
+    font-size: 3rem;
+    color: var(--color-text-mute);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
 </style>
